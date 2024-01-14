2b7d4d72c1b605be311372b409edc96e358a2040
Michael Schmidt <michael.schmidt@wuerth-it.com>
2024-01-14 20:28:16 +0100

    Using authenticated MQTT server

--- mqttHandler.js
+++ mqttHandler.js
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import fs from 'fs'
 
 const idMapFile = './idMap.json'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 
 export class MqttHandler {
   idMap
@@ -13,14 +14,17 @@ export class MqttHandler {
     this.influxService = influxService
     this.idMap = JSON.parse(fs.readFileSync(idMapFile))
 
-    const client = mqtt.connect(process.env.MQTT_URL)
+    const client = mqtt.connect(process.env.MQTT_URL, {
+      username: process.env.MQTT_USER,
+      password: process.env.MQTT_PASSWORD
+    })
     client.on('message', this.handleTemperaturUpdate.bind(this))
     client.on('connect', () => {
       console.log('MQTT connected')
       client.subscribe('cxtTest')
     })
     client.on('error', (error) => {
-      console.error(error)
+      console.error('Failed to connect to MQTT', error)
     })
   }
 
