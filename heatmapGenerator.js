4aed2bda2b56d349a73b7aa9306b74565770af10
Michael Schmidt <michael.schmidt@wuerth-it.com>
2024-01-14 11:55:54 +0100

    Refactoring of Heatmap Generator

--- heatmapGenerator.js
+++ heatmapGenerator.js
@@ -4,32 +4,61 @@ import sharp from 'sharp'
 import { saalplanMap } from './saalplanMap.js'
 import fs from 'fs'
 
-export const createHeatmap = (currentTemperatures) => {
-  const temperaturePoints = []
+const bestuhlungsplan = './bestuhlungsplan_cxt25.png'
 
-  for (const [key, value] of Object.entries(currentTemperatures)) {
-    const seatLocation = saalplanMap[key]
-    const x = seatLocation.x
-    const y = seatLocation.y
-    const v = value
-    temperaturePoints.push({ x: x, y: y, value: v })
+const currentTemperatures = {
+  A18: 25,
+  Entertainment3: 20,
+  D3: 28,
+  D19: 27,
+  H1: 35,
+  J3: 34,
+  J19: 33,
+  K2: 34,
+  L17: 33,
+  M4: 28,
+  N18: 26,
+  P5: 18,
+  P17: 26,
+  Helpdesk8: 22,
+  S12: 39,
+  T20: 17
+}
+
+export class HeatmapGenerator {
+  targetFile = ''
+  constructor(targetFile) {
+    this.targetFile = targetFile
+    this.createHeatmap(currentTemperatures)
+    setInterval(() => {
+      this.createHeatmap(currentTemperatures)
+    }, 5000)
   }
 
-  const canvas = createCanvas(1903, 1124)
-  const ctx = canvas.getContext('2d')
-  const drw = new TemperatureMap(ctx)
-  drw.setPoints(temperaturePoints, 1903, 1124)
-  drw.drawFull(false, function () {
-    drw.drawPoints(function () {
-      sharp('./bestuhlungsplan_cxt25.png')
-        .composite([{ input: canvas.toBuffer(), gravity: 'center' }])
-        .toFile('./temp.png', (err, info) => {
-          if (!err) {
-            fs.copyFile('./temp.png', './finished.png', (err) => {
-              if (err) throw err
-            })
-          }
-        })
+  createHeatmap(currentTemperatures) {
+    const temperaturePoints = []
+
+    for (const [key, value] of Object.entries(currentTemperatures)) {
+      const seatLocation = saalplanMap[key]
+      const x = seatLocation.x
+      const y = seatLocation.y
+      const v = value
+      temperaturePoints.push({ x: x, y: y, value: v })
+    }
+
+    const canvas = createCanvas(1903, 1124)
+    const ctx = canvas.getContext('2d')
+    const drw = new TemperatureMap(ctx)
+    drw.setPoints(temperaturePoints, 1903, 1124)
+    drw.drawFull(false, () => {
+      drw.drawPoints(async () => {
+        await sharp(bestuhlungsplan)
+          .composite([{ input: canvas.toBuffer(), gravity: 'center' }])
+          .toFile('./temp.png')
+
+        fs.copyFileSync('./temp.png', this.targetFile)
+        fs.rmSync('./temp.png')
+      })
     })
-  })
+  }
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createHeatmap } from './heatmapGenerator.js'
+import { HeatmapGenerator } from './heatmapGenerator.js'
 import express from 'express'
 import { fileURLToPath } from 'url'
 import path from 'path'
@@ -7,24 +7,7 @@ import { PrometheusHandler } from './prometheusHandler.js'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const currentTemperatures = {
-  A18: 25,
-  Entertainment3: 20,
-  D3: 28,
-  D19: 27,
-  H1: 35,
-  J3: 34,
-  J19: 33,
-  K2: 34,
-  L17: 33,
-  M4: 28,
-  N18: 26,
-  P5: 18,
-  P17: 26,
-  Helpdesk8: 22,
-  S12: 39,
-  T20: 17
-}
+const targetFile = './finished.png'
 
 //startMqttHandler()
 
@@ -34,14 +17,11 @@ const port = 3000
 new PrometheusHandler(app)
 
 app.get('/', (req, res) => {
-  res.sendFile('./finished.png', { root: __dirname })
+  res.sendFile(targetFile, { root: __dirname })
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-createHeatmap(currentTemperatures)
-setInterval(() => {
-  createHeatmap(currentTemperatures)
-}, 5000)
+new HeatmapGenerator(targetFile)
